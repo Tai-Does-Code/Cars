d3fbb34f2c8b080e9eaaa1468b852b1bb32d613b
Taifinkaba <taifinkabad@gmail.com>
2025-01-29 19:57:28 -0500

    explore_cars shows all 30 cars option, next work on sign up and sign in forms(frontend)

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("5818907ef17b5815")
+/******/ 		__webpack_require__.h = () => ("f07db07c0387dc40")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/explore_cars/page.tsx
+++ app/explore_cars/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from 'react';
+import { CarParts } from '@/types';
+import CarCard from '../../components/CarCard';
+import { fetchCars } from '@/utils';
 
 const ExploreCars = () => {
-  return (
-    <div>welcome to the explore page</div>
-  )
-}
+  const [cars, setCars] = useState([]); 
+  const [isLoading, setIsLoading] = useState(true); 
+  const [error, setError] = useState<string | null>(null); 
 
-export default ExploreCars
+  useEffect(() => {
+    const getCars = async () => {
+      try {
+        const data = await fetchCars(); 
+        setCars(data || []); 
+        setIsLoading(false); 
+      } catch (err) {
+        setError("Failed to fetch car data.");
+        setIsLoading(false);
+      }
+    };
+
+    getCars(); 
+  }, []); 
+
+  return (
+    <div className='explore-cars-container'>
+      <h1 className='text-center text-2xl font-bold mb-6'>Explore Cars</h1>
+
+      <div className='car-cards-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+        {cars.length > 0 ? (
+          cars.map((car) => (
+            <CarCard key={car.id} car={car} />
+          ))
+        ) : (
+          <p>Loading cars...</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ExploreCars;
